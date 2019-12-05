e0815fbadf59a5d6130e56a269316d7a8a867329
NickolasDzR <nikolyae@mail.ru>
2019-12-05 11:27:25 +0300

    some new updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const startTheGame = () => {
-    console.log('Welcome to the brain-games!');
-
-    const actual = readlineSync.question('May i have your name? ');
-    console.log(`Hello ${actual}!`);
-}
+  console.log('Welcome to the brain-games!');
+  const actual = readlineSync.question('May i have your name? ');
+  console.log(`Hello ${actual}!`);
+};
 
 export default startTheGame;
