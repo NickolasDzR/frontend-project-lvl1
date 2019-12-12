086371af957929ee5b4c27a3a5ea7438aefce432
NickolasDzR <nikolyae@mail.ru>
2019-12-12 10:50:40 +0300

    brain-calc refactoring

--- src/Games/getCalc.js
+++ src/Games/getCalc.js
@@ -1,26 +1,13 @@
 import startTheGame from '..';
 import randomNumber from './randomNumber';
 
-const randomNumberSmall = () => Math.floor(Math.random() * 3);
 const message = 'What is the result of the expression?';
-
-const indication = () => {
-  switch (randomNumberSmall()) {
-    case 0:
-      return 0; // 0 = +
-    case 1:
-      return 1; // 1 = -
-    case 2:
-      return 2; // 2 = *
-    default:
-      return false;
-  }
-};
 
 const getTheRightAnswer = () => {
   const firstNumber = randomNumber();
   const secondNumber = randomNumber();
-  switch (indication()) {
+  const randomNumberSmall = Math.floor(Math.random() * 3);
+  switch (randomNumberSmall) {
     case 0:
       return [`${firstNumber} + ${secondNumber}`, firstNumber + secondNumber];
     case 1:
