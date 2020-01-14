93fe3ce66e74146d4129ec28d4932da392e8f437
NickolasDzR <nikolyae@mail.ru>
2020-01-14 20:12:28 +0300

    new updates

--- src/Games/progression.js
+++ src/Games/progression.js
@@ -4,20 +4,20 @@ import randomNumber from '../getRandomNumber/randomNumber';
 const gameDescription = 'What number is missing in the progression?';
 const progressionLenght = 10;
 
-const makeProgression = (fPositionArray, increaseNum, hiddenNum) => {
+const getQuestion = (firstPositionArray, increaseNum, hiddenNumIndex) => {
   const progression = [];
   for (let i = 0; i < progressionLenght; i += 1) {
-    progression.push(Math.abs(`${fPositionArray + (increaseNum * i)} `));
+    progression.push(firstPositionArray + (increaseNum * i));
   }
-  progression.splice(hiddenNum, 1, '..');
+  progression.splice(hiddenNumIndex, 1, '..');
   return progression.join(' ');
 };
 
 const generateGameData = () => {
   const firstPositionArray = randomNumber(1, 100);
   const increaseNum = randomNumber(1, 20);
-  const hiddenNumIndex = randomNumber(1, 9);
-  const question = makeProgression(firstPositionArray, increaseNum, hiddenNumIndex);
+  const hiddenNumIndex = randomNumber(1, progressionLenght - 1);
+  const question = getQuestion(firstPositionArray, increaseNum, hiddenNumIndex);
   const answer = firstPositionArray + (increaseNum * hiddenNumIndex);
   return [question, answer.toString()];
 };
