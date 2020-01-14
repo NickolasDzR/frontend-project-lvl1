336f2ee2f2dd7444755514f3ec8d237526cfb7b7
NickolasDzR <nikolyae@mail.ru>
2020-01-14 21:20:31 +0300

    fixed on variable

--- src/Games/progression.js
+++ src/Games/progression.js
@@ -4,21 +4,21 @@ import randomNumber from '../getRandomNumber/randomNumber';
 const gameDescription = 'What number is missing in the progression?';
 const progressionLenght = 10;
 
-const getQuestion = (firstPositionArray, increaseNum, hiddenNumIndex) => {
+const getQuestion = (firstNumber, increaseNum, hiddenNumIndex) => {
   const progression = [];
   for (let i = 0; i < progressionLenght; i += 1) {
-    progression.push(firstPositionArray + (increaseNum * i));
+    progression.push(firstNumber + (increaseNum * i));
   }
   progression.splice(hiddenNumIndex, 1, '..');
   return progression.join(' ');
 };
 
 const generateGameData = () => {
-  const firstPositionArray = randomNumber(1, 100);
+  const firstNumber = randomNumber(1, 100);
   const increaseNum = randomNumber(1, 20);
   const hiddenNumIndex = randomNumber(1, progressionLenght - 1);
-  const question = getQuestion(firstPositionArray, increaseNum, hiddenNumIndex);
-  const answer = firstPositionArray + (increaseNum * hiddenNumIndex);
+  const question = getQuestion(firstNumber, increaseNum, hiddenNumIndex);
+  const answer = firstNumber + (increaseNum * hiddenNumIndex);
   return [question, answer.toString()];
 };
 
